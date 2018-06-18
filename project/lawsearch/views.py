915b2aa3142c75f271f637a789382d39ac2ef2af
booboo <thankyouhonest@gmail.com>
2018-06-19 07:33:15 +0800

    first commit

---
+++ project/lawsearch/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def hello_view(request):
+    return render(request,'userpage.html', {
+        'data': "Hello Django ",
+    })
+
